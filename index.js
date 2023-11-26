ade2b1fb03fd002dcb58ee5bdce522dfbcd4ec0d
Shubham-Sharma1995 <shubhamsharma.bme20@itbhu.ac.in>
2023-11-26 18:18:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// const http= require('http') //handles serveers request and response
+
+
+// http.createServer((req,resp)=>{
+//     resp.write("<h1>Hello, this is SS</h1>");
+//     resp.end();
+// }).listen(4500); //createserver-takes whole function as a parameter
+
+const colors= require('colors')
+console.log("hello".red);
+
+
